465458cbf02ad44c49508dedc7247fd0491581ec
Shivani Sharma <shivani1406sharma@gmail.com>
2021-12-01 22:52:13 +0000

    new commit

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 // establishes a connection with the game server
 const net = require("net");
 const readline = require('readline');
+const {IP,PORT} = require('./constants');
 const connect = function () {
   const conn = net.createConnection({
-    host:'192.168.0.16', // IP address here,
-    port: 50541// PORT number here,
+    host:IP, // IP address here,
+    port: PORT// PORT number here,
   });
 
   conn.on("connect", () => {
---
+++ constants.js
@@ -0,0 +1,7 @@
+const IP ='192.168.0.16';
+const PORT =50541;
+
+module.exports = {
+  IP,
+  PORT,
+};
